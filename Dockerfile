25efb7487edd292b85bf40c9ed5df50362359bf3
rralcala <rralcala@gmail.com>
2019-11-09 00:55:20 -0800

    Added fan control

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:latest
 
 RUN pip3 install mysql-connector-python
-RUN pip3 install requests pytz
+RUN pip3 install requests pytz pyhs100
 COPY . /
 
 CMD ["python", "/stream.py"]
--- stream.py
+++ stream.py
@@ -1,16 +1,21 @@
 import datetime
 import json
 import logging
-import pytz
-import re
-import requests
-import time
-import select
-import socket
-
 import mysql.connector
 from mysql.connector import Error
 from mysql.connector import errorcode
+from pyHS100 import SmartBulb, SmartPlug
+import pytz
+import re
+import requests
+import select
+import socket
+import time
+from threading import Timer
+
+HEATING = "1"
+FAN_OFF_DELAY = 120.0
+
 
 def get_temp(api_key):
     # base_url variable to store url 
@@ -49,23 +54,44 @@ def get_temp(api_key):
     
     else: 
         logger.error(" City Not Found ")
-        return -100
+        return -273.15
 
+def fan_off():
+    plug = SmartPlug(FAN_IP)
+    plug.turn_off()
+
+def new_data(m, config, dt):
+    temp = m.group(1)
+    oper = m.group(2)
+    heat = m.group(3)
+    
+    otemp = get_temp(config["weather"])
+    date = dt.strftime("%Y-%m-%d %H:%M")
+    save_row(config['mysql'], date, otemp, temp, oper, heat)
+    return heat
+
+
+def handle_fan(heat):
+    if heat == HEATING:
+        SmartPlug(FAN_IP).turn_on()
+    else:
+        Timer(FAN_OFF_DELAY, fan_off).start()
 
 def poll(config):
-    
+    prev_state = 0
+    fan_off()
+    data = ""        
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        
         try:
             s.connect((config['temp-control']['host'], config['temp-control']['port']))
+            s.setblocking(0)
         except Exception as e:
             logger.error(e)
             time.sleep(10)
             return
         logger.info('Connected to {}'.format( config['temp-control']['host']))
-        data = ""
-        last_min =  datetime.datetime.now().minute - 1
-        s.setblocking(0)
+        
+        last_min = datetime.datetime.now().minute - 1
         while True:
             ready = select.select([s], [], [], 10.0)
             if ready[0]:
@@ -79,14 +105,11 @@ def poll(config):
                     m = re.search('([0-9]+\.[0-9]+)\s([01])\s([01])', newline)
                     dt = datetime.datetime.now(tz)
                     if m and last_min != dt.minute:
-                        temp = m.group(1)
-                        oper = m.group(2)
-                        heat = m.group(3)
+                        heat = new_data(m, config, dt)
+                        if prev_state != heat:
+                            handle_fan(heat)
+                            prev_state = heat
                         last_min = dt.minute
-                        otemp = get_temp(config["weather"])
-                        date = dt.strftime("%Y-%m-%d %H:%M")
-                        save_row(config['mysql'], date, otemp, temp, oper, heat)
-
                         data = ""
                     data = data[pos+2:]
             else:
@@ -114,7 +137,7 @@ def save_row(config, date, otemp, temp, oper, heat):
         cursor.close()
         logger.info(f"(\"{date}\", \"{otemp}\", \"{temp}\", {oper}, {heat})")
     except mysql.connector.Error as error:
-        logger.error("Failed to insert record into Laptop table {}".format(error))
+        logger.error("Failed to insert record {}".format(error))
 
     finally:
         if (connection and connection.is_connected()):
@@ -125,7 +148,7 @@ if __name__ == "__main__":
 
     with open("config/config.json") as conf_file:
         config = json.load(conf_file)
-
+    FAN_IP = config["fan"]
     tz = pytz.timezone("US/Pacific")
     logging.basicConfig(level=logging.INFO)
     
